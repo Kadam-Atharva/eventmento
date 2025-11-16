c6a067d9f1d9b9489a1f9aa94e39ac9a9e7e3f9d
Atharva Kadam <atharvakadam751@gmail.com>
2025-11-16 12:28:00 +0530

    First Commit

--- src/main/java/com/atharva/eventmento/config/SecurityConfig.java
+++ src/main/java/com/atharva/eventmento/config/SecurityConfig.java
@@ -15,17 +15,20 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(
-            HttpSecurity http, UserProvisioningFilter userProvisioningFilter) throws Exception{
+            HttpSecurity http,
+            UserProvisioningFilter userProvisioningFilter,
+            JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception{
         http
                 .authorizeHttpRequests(authorize ->
                         authorize
-                                .requestMatchers(HttpMethod.GET, "/api/v1/published-events").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll()
                                 .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(Customizer.withDefaults()))
+                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter
+                        )))
                 .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
 
         return http.build();
