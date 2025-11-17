4eef88038267750f2678fdd227819375d5b66c9c
Atharva Kadam <atharvakadam751@gmail.com>
2025-11-17 22:19:59 +0530

    First Commit

---
+++ src/main/java/com/atharva/eventmento/repositories/TicketRepository.java
@@ -0,0 +1,17 @@
+package com.atharva.eventmento.repositories;
+
+import com.atharva.eventmento.domain.entities.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+    int countByTicketTypeId(UUID ticketTypeId);
+
+    Page<Ticket> findByPurchaserId(UUID purchaserId, Pageable pageable);
+
+}
