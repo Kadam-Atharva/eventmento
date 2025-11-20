c688176a2fc6dbfa2cbd6bf34f97f07e790efee5
Atharva Kadam <atharvakadam751@gmail.com>
2025-11-20 11:48:42 +0530

    First Commit

---
+++ src/main/java/com/atharva/eventmento/domain/CreateEventRequest.java
@@ -0,0 +1,27 @@
+package com.atharva.eventmento.domain;
+
+import com.atharva.eventmento.domain.entities.EventStatusEnum;
+import com.atharva.eventmento.domain.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequest {
+
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+    private List<CreateTicketTypeRequest> ticketTypes = new ArrayList<>();
+
+}
