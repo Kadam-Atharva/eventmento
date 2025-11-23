d207eb7f458e58f030358a59f1138eee1866a791
Atharva Kadam <atharvakadam751@gmail.com>
2025-11-23 10:05:10 +0530

    Second Commit

---
+++ src/main/java/com/atharva/eventmento/services/impl/TicketValidationServiceImpl.java
@@ -0,0 +1,55 @@
+package com.atharva.eventmento.services.impl;
+
+import com.atharva.eventmento.domain.entities.*;
+import com.atharva.eventmento.exceptions.QrCodeNotFoundException;
+import com.atharva.eventmento.exceptions.TicketNotFoundException;
+import com.atharva.eventmento.repositories.QrCodeRepository;
+import com.atharva.eventmento.repositories.TicketRepository;
+import com.atharva.eventmento.repositories.TicketValidationRepository;
+import com.atharva.eventmento.services.TicketValidationService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class TicketValidationServiceImpl implements TicketValidationService {
+
+    private final QrCodeRepository qrCodeRepository;
+    private final TicketValidationRepository ticketValidationRepository;
+    private final TicketRepository ticketRepository;
+
+    @Override
+    public TicketValidation validateTicketByQrCode(UUID qrCodeId) {
+        QrCode qrCode = qrCodeRepository.findByIdAndStatus(qrCodeId, QrCodeStatusEnum.ACTIVE)
+                .orElseThrow(() -> new QrCodeNotFoundException(
+                        String.format("Qr Code with Id %s was not found", qrCodeId)
+                ));
+        Ticket ticket = qrCode.getTicket();
+        return validateTicket(ticket, TicketValidationMethod.QR_SCAN);
+    }
+
+    private TicketValidation validateTicket(Ticket ticket, TicketValidationMethod ticketValidationMethod) {
+        TicketValidation ticketValidation = new TicketValidation();
+        ticketValidation.setTicket(ticket);
+        ticketValidation.setValidationMethod(ticketValidationMethod);
+        TicketValidationStatusEnum ticketValidationStatus = ticket.getValidations().stream()
+                .filter(v -> TicketValidationStatusEnum.VALID.equals(v.getStatus()))
+                .findFirst()
+                .map(v -> TicketValidationStatusEnum.INVALID)
+                .orElse(TicketValidationStatusEnum.VALID);
+
+        ticketValidation.setStatus(ticketValidationStatus);
+        return ticketValidationRepository.save(ticketValidation);
+    }
+
+    @Override
+    public TicketValidation validateTicketManually(UUID ticketId) {
+        Ticket ticket = ticketRepository.findById(ticketId)
+                .orElseThrow(TicketNotFoundException::new);
+        return validateTicket(ticket, TicketValidationMethod.MANUAL);
+    }
+}
