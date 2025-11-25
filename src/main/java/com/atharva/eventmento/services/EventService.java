6ecd9b3bab84af1ae8ba7e9df7e2982f4b83eaad
Atharva Kadam <atharvakadam751@gmail.com>
2025-11-25 13:22:29 +0530

    Second Commit

--- src/main/java/com/atharva/eventmento/services/EventService.java
+++ src/main/java/com/atharva/eventmento/services/EventService.java
@@ -2,6 +2,7 @@ package com.atharva.eventmento.services;
 
 import com.atharva.eventmento.domain.CreateEventRequest;
 import com.atharva.eventmento.domain.UpdateEventRequest;
+import com.atharva.eventmento.domain.dtos.EventParticipantsResponseDto;
 import com.atharva.eventmento.domain.entities.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,4 +19,6 @@ public interface EventService {
     Page<Event> listPublishedEvents(Pageable pageable);
     Page<Event> searchPublishedEvents(String query, Pageable pageable);
     Optional<Event> getPublishedEvent(UUID id);
+    void addStaffToEvent(UUID organizerId, UUID eventId, String staffEmail);
+    EventParticipantsResponseDto getEventParticipants(UUID eventId, UUID requestingUserId);
 }
