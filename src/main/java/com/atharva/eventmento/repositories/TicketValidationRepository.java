934007a0d6f0e35c41a04a768948a05817c47d85
Atharva Kadam <atharvakadam751@gmail.com>
2025-12-01 11:16:01 +0530

    Second Commit

---
+++ src/main/java/com/atharva/eventmento/repositories/TicketValidationRepository.java
@@ -0,0 +1,11 @@
+package com.atharva.eventmento.repositories;
+
+import com.atharva.eventmento.domain.entities.TicketValidation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TicketValidationRepository extends JpaRepository<TicketValidation, UUID> {
+}
