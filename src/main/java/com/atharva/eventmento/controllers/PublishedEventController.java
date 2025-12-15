075510be60fa397da676cce74cdaa4796daceee6
Atharva Kadam <atharvakadam751@gmail.com>
2025-12-15 15:50:39 +0530

    Third Commit

---
+++ src/main/java/com/atharva/eventmento/controllers/PublishedEventController.java
@@ -0,0 +1,49 @@
+package com.atharva.eventmento.controllers;
+
+import com.atharva.eventmento.domain.dtos.GetPublishedEventDetailsResponseDto;
+import com.atharva.eventmento.domain.dtos.ListPublishedEventResponseDto;
+import com.atharva.eventmento.domain.entities.Event;
+import com.atharva.eventmento.mappers.EventMapper;
+import com.atharva.eventmento.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/v1/published-events")
+@RequiredArgsConstructor
+public class PublishedEventController {
+
+    private final EventService eventService;
+    private final EventMapper eventMapper;
+
+    @GetMapping
+    public ResponseEntity<Page<ListPublishedEventResponseDto>> listPublishedEvent(
+            @RequestParam(required = false) String q,
+            Pageable pageable) {
+        Page<Event> events;
+        if(null != q && !q.trim().isEmpty()) {
+            events = eventService.searchPublishedEvents(q, pageable);
+        } else {
+            events = eventService.listPublishedEvents(pageable);
+        }
+
+        return ResponseEntity.ok(
+                events.map(eventMapper::toListPublishedEventResponseDto));
+    }
+
+    @GetMapping(path = "/{eventId}")
+    public ResponseEntity<GetPublishedEventDetailsResponseDto> getPublishedEventDetails(
+            @PathVariable UUID eventId
+            ) {
+        return eventService.getPublishedEvent(eventId)
+                .map(eventMapper::toGetPublishedEventDetailsResponseDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
