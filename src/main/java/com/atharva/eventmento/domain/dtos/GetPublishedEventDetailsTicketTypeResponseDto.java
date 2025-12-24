73aaf7c43cb0c99ae839bb4ecdd8f1ed28f45ee2
Atharva Kadam <atharvakadam751@gmail.com>
2025-12-24 11:28:58 +0530

    Third Commit

---
+++ src/main/java/com/atharva/eventmento/domain/dtos/GetPublishedEventDetailsTicketTypeResponseDto.java
@@ -0,0 +1,19 @@
+package com.atharva.eventmento.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetPublishedEventDetailsTicketTypeResponseDto {
+
+    private UUID id;
+    private String name;
+    private Double price;
+    private String description;
+}
