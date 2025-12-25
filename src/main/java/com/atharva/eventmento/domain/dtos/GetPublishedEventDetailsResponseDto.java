da9c97d7fbc1f7b960ac6df78432a348fc927421
Atharva Kadam <atharvakadam751@gmail.com>
2025-12-25 12:38:07 +0530

    Third Commit

---
+++ src/main/java/com/atharva/eventmento/domain/dtos/GetPublishedEventDetailsResponseDto.java
@@ -0,0 +1,24 @@
+package com.atharva.eventmento.domain.dtos;
+
+import com.atharva.eventmento.domain.entities.EventStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetPublishedEventDetailsResponseDto {
+
+    private UUID id;
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private List<GetPublishedEventDetailsTicketTypeResponseDto> ticketTypes = new ArrayList<>();
+}
