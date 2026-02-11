9976c6e9de392b7c8373f59d0bc74495b8cc7e26
Atharva Kadam <atharvakadam751@gmail.com>
2026-02-11 12:57:19 +0530

    fifth Commit

--- src/main/java/com/atharva/eventmento/domain/UpdateEventRequest.java
+++ src/main/java/com/atharva/eventmento/domain/UpdateEventRequest.java
@@ -24,5 +24,6 @@ public class UpdateEventRequest {
     private LocalDateTime salesEnd;
     private EventStatusEnum status;
     private List<UpdateTicketTypeRequest> ticketTypes = new ArrayList<>();
+    private String description;
     private String coverImage;
 }
